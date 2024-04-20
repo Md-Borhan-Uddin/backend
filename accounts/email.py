c2e444622e329fd228e8ed2df9c3c8bed92d1415
Borhan <mdborhanuddinab21@gmail.com>
2024-04-20 13:37:40 +0600

    update

--- accounts/email.py
+++ accounts/email.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.tokens import default_token_generator
 from templated_mail.mail import BaseEmailMessage
-from django.utils.encoding import smart_str, force_bytes
-from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
 
 
 class ActivationEmail(BaseEmailMessage):
